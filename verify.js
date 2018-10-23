fbf41e0ed63b7fc36191851ed7a36adc5106db65
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-10-23 08:59:35 -0500

    Added thankyou.html, styling, isValid(), and some comments.

--- verify.js
+++ verify.js
@@ -1,3 +1,19 @@
+/*
+Developers: Greg Pugh and Frankie Barrios
+Date:       10/23/18
+Purpose:    Javascript Form Validation
+*/
+function isValid(frm) {
+    var isValid = true;
+    var email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (frm.email.value == "" || frm.email.value != email) {
+        isValid = false;
+        frm.email.style.backgroundColor = "red";
+        document.getElementById("error").style.display = "block";
+    }
+}
+
 function validate(frm){
     var isValid = true;
     var phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
@@ -14,4 +30,9 @@ function validate(frm){
     }
     
 return isValid;
+}
+
+function valForm(){
+    isValid();
+    validate();
 }
