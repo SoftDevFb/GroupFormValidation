e4f3508b0c4e834189dc936f99f3bbe1bdb07ff5
PUGH <arbitwah@gmail.com>
2018-10-24 09:27:07 -0500

    awaiting merge error

--- verify.js
+++ verify.js
@@ -9,7 +9,7 @@ var isValid = true;
 
 //Checks Name Validity
 function nameValid(frm) {
-    var name = /[a-z A-Z]/;
+    var name = /^[a-zA-Z]/;
     if (name.test(frm.name.value) || frm.name.value == "") {
         isValid = false;
         frm.name.style.backgroundColor = "red";
@@ -20,15 +20,9 @@ function nameValid(frm) {
 
 //Checks Email Validity
 function emailValid(frm){
-<<<<<<< HEAD
-    var email = /[a - zA - Z0 -9_. + -]@[a - zA - Z0 - 9 -]\.[a - zA - Z0 - 9 -.]/ ;
-
-    if (email.test(frm.email.value)|| frm.email.value == "") {
-=======
     var email = /[a - zA - Z0 -9_. + -] @[a - zA - Z0 - 9 -]\.[a - zA - Z0 - 9 -.]/ ;
 
     if (email.test(frm.email.value) || frm.email.value == ""){
->>>>>>> dae52f9694eb9485ed5abe62924c7ff78b8b2422
       isValid = false;
       frm.email.style.backgroundColor = "red";
      // document.getElementById("error").style.display = "block";
